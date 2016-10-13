6f125a2ec91b955103a3262f628c8729a479302d
JobbyM <songzhipengmune@gmail.com>
2016-10-13 10:03:10 +0800

    add react demo

--- index.android.js
+++ index.android.js
@@ -1,53 +1,21 @@
 /**
  * Sample React Native App
- * https://github.com/facebook/react-native
+ * https://facebook.github.io/react-native/docs/tutorial.html#content
  * @flow
  */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Text
 } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          热烈欢迎React Native
-        </Text>
-        <Text style={styles.instructions}>
-          Hello World！你好，新世界！
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Text>热烈欢迎来到新世界！</Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
