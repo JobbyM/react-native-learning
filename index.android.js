14a430ee6097d55baef60b48c9822c1328b6d15d
JobbyM <songzhipengmune@gmail.com>
2016-10-18 13:55:58 +0800

    add react-native props

--- index.android.js
+++ index.android.js
@@ -7,15 +7,36 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Text
+  Image,
+  ListView,
+  Navigator,
+  NavigationExperimental,
+  PixelRatio,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View
 } from 'react-native';
 
-class AwesomeProject extends Component {
+class HelloWorld extends Component {
   render() {
     return (
-      <Text>热烈欢迎来到新世界！</Text>
+      <Text>欢迎来到新世界!</Text>
+    )
+  }
+}
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}} />
     );
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
