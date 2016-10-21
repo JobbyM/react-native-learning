b5fea791aaccf7961606895e3f244ccdc5b5647e
JobbyM <songzhipengmune@gmail.com>
2016-10-21 10:52:10 +0800

    add react-native flex dimension

--- index.android.js
+++ index.android.js
@@ -127,4 +127,16 @@ class FixedDimensionsBasics extends Component {
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
