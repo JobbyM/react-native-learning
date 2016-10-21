ba2fa5d1a11db43d3eb34c241517327e242ad676
JobbyM <songzhipengmune@gmail.com>
2016-10-21 10:51:02 +0800

    add react-native fixed dimension

--- index.android.js
+++ index.android.js
@@ -115,4 +115,16 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+class FixedDimensionsBasics extends Component {
+  render() {
+    return (
+      <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
