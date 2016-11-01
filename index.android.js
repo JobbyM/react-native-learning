a84d4046b72c838b5203b59d6561850292d5f6b5
JobbyM <songzhipengmune@gmail.com>
2016-11-01 11:35:02 +0800

    add react native alignitems

--- index.android.js
+++ index.android.js
@@ -157,4 +157,24 @@ class JustifyContentBasics extends Component {
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => JustifyContentBasics);
+class AlignItemsBasics extends Component {
+  render() {
+    return (
+      // Try setting `alignItems` to `flex-start`
+      // Try setting `justifyContent` to `flex-end`
+      // Try setting `flexDirection` to `row`
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
