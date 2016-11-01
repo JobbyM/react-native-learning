9e161333f721e24379d70b285478bb18f3edffac
JobbyM <songzhipengmune@gmail.com>
2016-11-01 11:27:11 +0800

    add react-native justifycontent

--- index.android.js
+++ index.android.js
@@ -139,4 +139,22 @@ class FlexDimensionsBasics extends Component {
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
+class JustifyContentBasics extends Component {
+  render() {
+    return (
+      // Try setting `justifyContent` to `center`
+      // Try setting `flexDirection` to `row`
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => JustifyContentBasics);
