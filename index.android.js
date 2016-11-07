2177ba0664a2f8fdbd061e113ba1cc43d7c5bd5a
JobbyM <songzhipengmune@gmail.com>
2016-11-07 10:17:41 +0800

    react native using a scrollview

--- index.android.js
+++ index.android.js
@@ -199,4 +199,44 @@ class PizzaTranslator extends Component {
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+class IScolledDownAndWhatHappenedNextShockedMe extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Text style={{fontSize: 96}}>If you like</Text>
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Text style={{fontSize: 96}}>Scrolling down</Text>
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Text style={{fontSize: 96}}>What is the best</Text>
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Text style={{fontSize: 96}}>Framework around?</Text>
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Image source={require('./img/favicon.png')} />
+        <Text style={{fontSize: 96}}>React Native</Text>
+      </ScrollView>
+    )
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => IScolledDownAndWhatHappenedNextShockedMe);
